834ad3b0edaa209ea5b9754888cbf354f969f478
WchrpWwtk <wchrp.wwtk@gmail.com>
2023-09-25 22:25:06 +0700

    Remove if else condition in else.

--- logical_test_2.py
+++ logical_test_2.py
@@ -44,29 +44,25 @@ def arabic_to_roman() -> str:
     if num == 1000:
         roman += "M"
     else:
-        if num >= 500:
-            while num > 499:
-                roman += dict_of_roman["900"] if num > 899 else dict_of_roman["500"]
-                num -= 900 if num > 899 else 500
-        if num >= 100:
-            while num > 99:
-                roman += dict_of_roman["400"] if num > 399 else dict_of_roman["100"]
-                num -= 400 if num > 399 else 100
-        if num >= 50:
-            while num > 49:
-                roman += dict_of_roman["90"] if num > 89 else dict_of_roman["50"]
-                num -= 90 if num > 89 else 50
-        if num >= 10:
-            while num > 9:
-                roman += dict_of_roman["40"] if num > 39 else dict_of_roman["10"]
-                num -= 40 if num > 39 else 10
-        if num > 4:
-            roman += dict_of_roman["9"] if num == 9 else dict_of_roman["5"]
-            num -= 9 if num > 8 else 5
-        else:
-            while num > 0:
-                roman += dict_of_roman["4"] if num == 4 else dict_of_roman["1"]
-                num -= 4 if num > 3 else 1
+        while num > 499:
+            roman += dict_of_roman["900"] if num > 899 else dict_of_roman["500"]
+            num -= 900 if num > 899 else 500
+        while num > 99:
+            roman += dict_of_roman["400"] if num > 399 else dict_of_roman["100"]
+            num -= 400 if num > 399 else 100
+        while num > 49:
+            roman += dict_of_roman["90"] if num > 89 else dict_of_roman["50"]
+            num -= 90 if num > 89 else 50
+        while num > 9:
+            roman += dict_of_roman["40"] if num > 39 else dict_of_roman["10"]
+            num -= 40 if num > 39 else 10
+
+        roman += dict_of_roman["9"] if num == 9 else dict_of_roman["5"]
+        num -= 9 if num == 9 else 5
+
+        while num > 0:
+            roman += dict_of_roman["4"] if num == 4 else dict_of_roman["1"]
+            num -= 4 if num == 4 else 1
 
     print(roman)
 
